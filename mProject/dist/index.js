cd5f8dd8f995c765878fded31043cf0f7cf0cb92
esreekarreddy <esreekarreddy@gmail.com>
2023-07-24 12:30:45 +0530

    configuration demo

--- mProject/dist/index.js
+++ mProject/dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 let button = document.getElementById("btn");
 let input = document.getElementById("todoinput");
+console.log(button.getAttribute("id"));
 button.addEventListener("click", function () {
     alert(input.value);
     input.value = "";
